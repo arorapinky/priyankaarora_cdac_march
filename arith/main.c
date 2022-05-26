f5444a9c11caf6806cdf7232a6ded38f571491c4
arorapinky <priyankaarora26998@gmail.com>
2022-05-26 20:46:32 +0530

    added linker file

--- arith/main.c
+++ arith/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
+    int result = 0; 
  printf ("arith demo\n");
+   result = add(10,20);
+   printf ("Addition result: %d \n", result); 
     return 0;
 }
