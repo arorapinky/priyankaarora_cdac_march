f176a22c9f703d47b0054d48a6598d35fb2ecd77
priyankaarora26 <priyankaarora26998@gmail.com>
2022-05-30 15:06:15 -0400

    changes made

---
+++ arithmake/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int add (int, int);
+int main(int argc, char const *argv[])
+{
+    int result = 0; 
+ printf ("arith demo\n");
+   result = add(10,20);
+      
+   printf ("Addition result: %d \n", result); 
+
+    return 0;
+}
