d030215d42c23e0553bd2f70feff6cea46a5de43
priyankaarora26 <priyankaarora26998@gmail.com>
2022-05-30 15:34:43 -0400

    changes made

---
+++ arith/day_3/libsys_call/libsysdemo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+
+char str[]="Library and system calls";
+
+ int main(int argc, char const *argv[])
+ 
+ {
+
+     int len=strlen(str);
+    printf(" length = %d\n",len);
+
+
+     return 0;
+ }
+ 
